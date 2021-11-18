085bd4b3382b1c2f0ab3b4254a976ec2d6a83555
Chandler Burket <chandlerburket@gmail.com>
2021-11-18 15:32:11 -0500

    Added NavBar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar"
+import MainContent from "./MainContent";
+export default class App extends Component
+{
+    render(){
+        return <React.Fragment>
+            <NavBar />
+            <MainContent />
+        </React.Fragment>;
+    }
+}
---
+++ src/MainContent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class MainContent extends Component 
+{
+    render()
+    {
+        return <div>Hello from Main Content</div>
+    }
+}
